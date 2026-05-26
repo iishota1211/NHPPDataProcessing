2e7dfc1c91f9f734351c5bf906e257548b0b6528
iiShota <iishota1211@gmail.com>
2026-05-26 12:41:41 +0900

    add functions for reading and writing json files

--- data_processor.py
+++ data_processor.py
@@ -4,6 +4,11 @@ import json
 from datetime import datetime
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
+base_dir = os.path.join(current_dir, "filtered_data")
+
+cve_source_folder = os.path.join(base_dir, "filtered_cves")
+cve_source_file_name = "filtered_nvdcve-2.0-2026.json"
+cve_source_dir = os.path.join(cve_source_folder, cve_source_file_name)
 
 def extract_vendor_product(cpe_string):
     parts = cpe_string.split(":")
@@ -57,8 +62,8 @@ def validate_version(cpe_string, target_vendor, target_product):
         print("Invalid string: cannot convert to integer")
         return None
 
-def find_unique_versions(cves,target_vendor, target_product):
-    versions = []
+def filter_date_and_versions(cves,target_vendor, target_product):
+    filtered_data = {}
     for cve in cves:
         configurations = cve.get("configurations", {})
         nodes = []
@@ -70,45 +75,64 @@ def find_unique_versions(cves,target_vendor, target_product):
         for cpeMatch in cpeMatches:
             cpe_string = cpeMatch.get("criteria", "")
             version = validate_version(cpe_string, target_vendor, target_product)
-            if version is not None and version not in versions:
-                versions.append(version)
-    return versions
-
-filtered_data_folder = os.path.join(current_dir, "filtered_data")
-
-def save_extracted_data(data, filename):
-    if not os.path.exists(filtered_data_folder):
-        os.makedirs(filtered_data_folder)
-    file_path = os.path.join(filtered_data_folder, filename)
-    with open(file_path, 'w', encoding='utf-8') as file:
-        json.dump(data, file, indent=4)
-
-current_dir = os.path.dirname(os.path.realpath(__file__))
-
-source_folder_name = os.path.join("filtered_data","fedora")
-source_folder = os.path.join(current_dir, source_folder_name)
-source_file_name = "filtered_nvdcve-2.0-2026.json"
-source_dir = os.path.join(current_dir, source_folder_name, source_file_name)
+            target_vendor, target_product = extract_vendor_product(cpe_string)
+            if target_vendor != target_vendor or target_product != target_product:
+                continue
+            if version is None:
+                continue
+            key = f"version_{version}"
+            value = cve.get("published")
+            filtered_data = append_data(filtered_data, key, value)
+    return filtered_data
 
 
+def save_json_data(data, file_dir_name, file_name):
+    target_dir = os.path.join(base_dir, file_dir_name)
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+    file_path = os.path.join(target_dir, file_name)
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+                json.dump(data, file, indent=4)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
 
-with open(source_dir, 'r', encoding='utf-8') as f:
-    data = json.load(f)
+def load_json_data(file_dir_name, file_name):
+    data = None
+    file_path = os.path.join(base_dir, file_dir_name, file_name)
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        data = None
+    return data
+
+def append_data(dict,key,value):
+    if key in dict:
+        dict[key].append(value)
+    else:
+        dict[key] = [value]
+    return dict
+
+def main():
+    data = load_json_data("filtered_cves", "filtered_nvdcve-2.0-2002.json")
+    filtered_json_data = load_json_data("filtered_details", "filtered_data.json")
     cves = data
-    print(f"cve length : {len(data)}")
+    # print(f"json details: {json.dumps(data, indent=4)}")
     filtered_cves = list(filter(lambda x: filter_cve(x, "fedoraproject", "fedora"),cves))
-    versions = find_unique_versions(filtered_cves, "fedoraproject", "fedora")
+    filtered_data = filter_date_and_versions(filtered_cves, "fedoraproject", "fedora")
     print(f"Filtered CVEs: {len(filtered_cves)}")
-    print(f"Unique Versions: {len(versions)}")
-    print(f"Versions: {versions}")
+    print(f"Filtered Data: {len(filtered_data)}")
 
-test_data = {}
-for i in range(1,10):
-    version_key = f"version_{i}"
-    test_data[version_key] = []
-if test_data.get("version_20") is not None:
-    test_data["version_20"].append("CVE-2026-0002")
-else:
-    test_data["version_20"] = ["CVE-2026-0002"]
+    print(f"Filtered Data: {json.dumps(filtered_data, indent=4)}")
+    for key in filtered_data.keys():
+        for value in filtered_data[key]:
+            print(f"{key}: {value}")
+            print(f"type of value: {type(value)}")
+    # save_json_data(filtered_json_data, "filtered_details", "filtered_data.json")
+    print(f"Filtered json Data: {json.dumps(filtered_json_data, indent=4)}")
 
-save_extracted_data(test_data, "test.json")
+
+if __name__=="__main__":
+    main()
