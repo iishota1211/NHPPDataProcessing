c3b299b4a9f9d88badbacd40ff28434653cddbfe
iiShota <iishota1211@gmail.com>
2026-06-01 18:33:28 +0900

    fix data merging function and add some print functions for debugging

--- scripts/test.py
+++ scripts/test.py
@@ -8,6 +8,9 @@ import config
 from test_functions import *
 
 modelNameList 	= ["Exp","Gamma","Pareto","LogEVMin","Plaw","Log"]
+modelNameList 	= ["Exp","Pareto","Plaw","Log"]
+
+model_with_parameter_c_list = ["Gamma","Pareto","LogEVMin"]
 dataSetDict = usrDataProcess.loadData(config.segmentPatternFlag, config.normalFlag)
 modelType = "typeI"
 dataType = "time"
@@ -34,7 +37,7 @@ def testLLF(methodDict, modelDict, dataType, dataDict):
         print(f"(Time) {nowTime} : (Predicted Value) {predNum}")
     print("\n")
 
-    pmse = calculatePMSE(predList, actualList)
+    pmse = calculatePMSE(culFormatedTestData, meanValueFun, paraList)
     print(f"PMSE: {pmse}")
 
     resultDict = dict()
@@ -65,42 +68,78 @@ def mergeDataSetByVersion(dataSetDict):
 
     FinalFormatedTrainDataList = []
     FinalFormatedTestData = []
+    CurrentTotalValue = 0
     for dataName in dataSetDict[dataType][predInterval]:
         culFormatedTrainData = dataSetDict[dataType][predInterval][dataName]["culFormatedTrainData"]
         culFormatedTestData = dataSetDict[dataType][predInterval][dataName]["culFormatedTestData"]
 
         mergedFormatedData = culFormatedTrainData + culFormatedTestData
         if dataName != lastDataName:
-            FinalFormatedTrainDataList.append(mergedFormatedData)
+            FinalMergedFormatedData = addExtraValueToDataset(mergedFormatedData, CurrentTotalValue)
+            CurrentTotalValue = FinalMergedFormatedData[-1][1]
+            FinalFormatedTrainDataList.append(FinalMergedFormatedData)
         else:
-            FinalFormatedTestData = mergedFormatedData
+            FinalMergedFormatedData = addExtraValueToDataset(mergedFormatedData, CurrentTotalValue)
+            FinalFormatedTestData = FinalMergedFormatedData
 
     return FinalFormatedTrainDataList, FinalFormatedTestData
 
-def calculatePMSE(predList, actualList):
+def addExtraValueToDataset(dataSet, extraValue):
+    newDataSet = []
+    for nowTime, nowNum in dataSet:
+        newDataSet.append((nowTime, nowNum + extraValue))
+    return newDataSet
+
+def calculatePMSE(culFormatedTestData, meanValueFun, paraList):
+    predList = []
+    actualList = []
+    
+    for nowTime, nowNum in culFormatedTestData:
+        predNum = meanValueFun(nowTime, paraList)
+        predList.append(predNum)
+        actualList.append(nowNum)
+
     if len(predList) != len(actualList):
         raise ValueError("Length of predicted list and actual list must be the same.")
     
     mse = sum((pred - actual) ** 2 for pred, actual in zip(predList, actualList)) / len(predList)
     return mse
 
+def displayDifference(culFormatedTestData, meanValueFun, paraList):
+    for nowTime, nowNum in culFormatedTestData:
+        predNum = meanValueFun(nowTime, paraList)
+        print(f"(Time) {nowTime} : (Predicted Value) {predNum}, (Actual Value) {nowNum}, (Difference) {predNum - nowNum}")
+
+def displayTrainDataSets(culFormatedTrainDataList):
+    for culFormatedTrainData in culFormatedTrainDataList:
+        for nowTime, nowNum in culFormatedTrainData:
+            print(f"(Time) {nowTime} : (Actual Value) {nowNum}")
+
+def displayTestDataSet(culFormatedTestData):
+    for nowTime, nowNum in culFormatedTestData:
+        print(f"(Time) {nowTime} : (Actual Value) {nowNum}")
+
 
 for predInterval in dataSetDict[dataType]:
 
     culFormatedTrainDataList, culFormatedTestData = mergeDataSetByVersion(dataSetDict)
-    lastCulFormatedTrainData = culFormatedTrainDataList[-1]
 
     for modelName in modelNameList:
         modelDict = dict()
         modelDict["modelType"] = modelType
         modelDict["modelName"] = modelName
         methodDict = None
+        meanValueFun, meanValueFun_backup = modelTool.meanValueFunction(modelDict)
     
         print(f"Testing model: {modelName}")
         paraList_scipy = calculate_parameters(modelDict, dataType, culFormatedTrainDataList)
-        print(f"Estimated parameters for model {modelName} using scipy.optimize: {paraList_scipy}")
-    #     resultDict = testLLF(methodDict, modelDict, dataType, DataDict)
-    #     resultList.append((modelName, resultDict))
-
-    # for modelName, resultDict in resultList:
-    #     print(f"Model: {modelName}, AIC: {resultDict['AIC']}, PMSE: {resultDict['PMSE']}")
+        pmse = []
+        if paraList_scipy is None:
+            print(f"Model {modelName} does not support parameter estimation yet.")
+        elif modelName in model_with_parameter_c_list:
+            for para in paraList_scipy:
+                pmse.append(calculatePMSE(culFormatedTestData, meanValueFun, para))
+                print(f"PMSE for model {modelName} with parameters {para}: {pmse[-1]}\n")
+        else:
+            pmse = calculatePMSE(culFormatedTestData, meanValueFun, paraList_scipy)
+            print(f"PMSE for model {modelName} with parameters {paraList_scipy}: {pmse}\n")
